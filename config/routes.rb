093e85551305580bedae229b7adbaf196e0e8506
rily53 <hori.ri77@gmail.com>
2022-03-16 15:51:37 +0900

    ③ユーザー管理機能-ルーティング＆コントローラー設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'homes/index'
   root to: "homes#index"
   devise_for :users
+  resources :users, only: [:edit, :update, :show]
   # get 'devise/sessions'
   # root to: new_user_session_path
   # get 'tasks/index'
