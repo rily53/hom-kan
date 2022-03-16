2276323908233809766eddf2eaa0c3db1453c53c
rily53 <hori.ri77@gmail.com>
2022-03-16 14:29:22 +0900

    ③ユーザー管理機能-ユーザー登録設定＆css修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers'
+
+  has_many   :home_users
+  has_many   :homes, through: :home_users
   has_many   :tasks
   has_many   :lists
   has_many   :memos
