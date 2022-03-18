a56dde57dde09e5e22dd3fe05df7b821cca79fcf
rily53 <hori.ri77@gmail.com>
2022-03-19 02:24:11 +0900

    ⑤ホーム管理機能-バリデーション設定＆DB設計変更

--- app/models/home.rb
+++ app/models/home.rb
@@ -1,5 +1,7 @@
 class Home < ApplicationRecord
 
+  validates :home_name, presence: true, uniquness: true, length: { minimum: 5, maximum: 20, with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/ }
+
   has_many   :home_users
   has_many   :users, through: :home_users
   has_many   :tasks
