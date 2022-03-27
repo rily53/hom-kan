bd200daecb1d91a428587e31d3e2a0518956e4a0
rily53 <hori.ri77@gmail.com>
2022-03-28 01:53:55 +0900

    CalendarFunction-AddColumn

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -1,8 +1,12 @@
 class Calendar < ApplicationRecord
 
   validates :title, presence: true
-  validates :schedule, presence: true
-  validates :start_time, presence: true
+  validate :schedule
+  validate :member
+  validates :start_date, presence: true
+  validate :start_time
+  validate :end_date
+  validate :end_time
 
   belongs_to :user
   belongs_to :home
