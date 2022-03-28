faa099f4140bd7e65d2e07b1e97c2ff94b952e41
rily53 <98877715+rily53@users.noreply.github.com>
2022-03-29 01:33:13 +0900

    CalendarFunction-Controller

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,23 +1,24 @@
 class CalendarsController < ApplicationController
+  before_action :set_item
 
   def index
-    @home = Home.find_by(id: params[:home_id])
+    # @home = Home.find_by(id: params[:home_id])
     @calendars = Calendar.where(@home_id).all
     @calendar = current_user.calendars.new
   end
 
   def show
-    @home = Home.find_by(id: params[:home_id])
+    # @home = Home.find_by(id: params[:home_id])
     @calendar = Calendar.find(params[:id])
   end
 
   def new
     @calendar = current_user.calendar.new
-    @home = Home.find_by(id: params[:home_id])
+    # @home = Home.find_by(id: params[:home_id])
   end
 
   def create
-    @home = Home.find_by(params[:home_id])
+    # @home = Home.find_by(params[:home_id])
     @calendar = current_user.calendars.new(calendar_params)
     @calendar.home_id = params[:home_id]
     if @calendar.save
@@ -28,17 +29,17 @@ class CalendarsController < ApplicationController
   end
 
   def edit
-    @home = Home.find_by(id: params[:home_id])
+    # @home = Home.find_by(id: params[:home_id])
     @calendar = Calendar.find(params[:id])
   end
 
   def update
-    @home = Home.find_by(id: params[:home_id])
+    # @home = Home.find_by(id: params[:home_id])
     @calendar = Calendar.find(params[:id])
   end
 
   def destroy
-    @home = Home.find_by(id: params[:home_id])
+    # @home = Home.find_by(id: params[:home_id])
     @calendar = Calendar.find(params[:id])
   end
 
@@ -46,6 +47,10 @@ class CalendarsController < ApplicationController
 
   def calendar_params
     params.require(:calendar).permit(:title, :schedule, :start_date, :start_time, :end_date, :end_time).merge(user_id: current_user.id, home_id: params[:home_id])
+  end
+
+  def set_item
+    @home = Home.find_by(id: params[:home_id])
   end
 
 end
