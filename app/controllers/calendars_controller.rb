b6fd914fda07b34ce905984d30bf66220c2bbd12
rily53 <98877715+rily53@users.noreply.github.com>
2022-03-30 16:39:35 +0900

    CalendarFunction-Organize

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -2,24 +2,21 @@ class CalendarsController < ApplicationController
   before_action :set_item
 
   def index
-    # @home = Home.find_by(id: params[:home_id])
-    @calendars = Calendar.where(@home_id).all
+    @calendars = @home.calendars.order(start_date: :ASC).limit(10)
+    # home_idとcalendarテーブル一致データを取得 今後：並び順(本日〜)
     @calendar = current_user.calendars.new
   end
 
   def show
-    # @home = Home.find_by(id: params[:home_id])
     @calendar = Calendar.find(params[:id])
   end
 
   def new
-    @calendar = current_user.calendar.new
-    # @home = Home.find_by(id: params[:home_id])
+    @calendar = current_user.calendars.new
   end
 
   def create
-    # @home = Home.find_by(params[:home_id])
-    @calendar = current_user.calendars.new(calendar_params)
+    @calendar = current_user.calendar.new(calendar_params)
     @calendar.home_id = params[:home_id]
     if @calendar.save
       redirect_to home_calendars_path(@home)
@@ -29,28 +26,28 @@ class CalendarsController < ApplicationController
   end
 
   def edit
-    # @home = Home.find_by(id: params[:home_id])
     @calendar = Calendar.find(params[:id])
   end
 
   def update
-    # @home = Home.find_by(id: params[:home_id])
     @calendar = Calendar.find(params[:id])
   end
 
   def destroy
-    # @home = Home.find_by(id: params[:home_id])
     @calendar = Calendar.find(params[:id])
   end
 
   private
 
   def calendar_params
-    params.require(:calendar).permit(:title, :schedule, :start_date, :start_time, :end_date, :end_time).merge(user_id: current_user.id, home_id: params[:home_id])
+    params.require(:calendar).permit(:title, :schedule, :member, :start_date, :start_time, :end_date, :end_time).merge(user_id: current_user.id, home_id: params[:home_id])
   end
 
   def set_item
-    @home = Home.find_by(id: params[:home_id])
+    # @home = Home.find_by(id: params[:home_id])
+    # 調べたコード
+    @home = Home.find(params[:home_id])
+    # Homeテーブルからhome_idカラムを取得 "id"のみだとエラー
   end
 
 end
