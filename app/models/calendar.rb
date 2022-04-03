2ccabe4d08b97f72142e5d4aa27a0ae0e836f637
rily53 <98877715+rily53@users.noreply.github.com>
2022-04-03 19:10:49 +0900

    CalendarFunction-Validation

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -1,5 +1,8 @@
 class Calendar < ApplicationRecord
 
+  belongs_to :user
+  belongs_to :home
+
   validates :title, presence: true
   validate :schedule
   validate :member
@@ -7,18 +10,22 @@ class Calendar < ApplicationRecord
   validate :s_time
   validate :end_date
   validate :e_time
-
-  belongs_to :user
-  belongs_to :home
+  validate :start_end_check
 
   def start_time
     self.start_date
     # カレンダーにイベント表示
-    
   end
 
   def end_time
     self.end_date
+  end
+
+  def start_end_check
+    if start_date.present?
+    errors.add(:end_date, "cannot be registered before start date") unless
+    self.start_date < self.end_date
+    end
   end
 
 end
