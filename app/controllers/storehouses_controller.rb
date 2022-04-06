c695b25459c9722ffce22d96b18051047b23df15
rily53 <98877715+rily53@users.noreply.github.com>
2022-04-06 11:29:02 +0900

    StorageFunction-View&transition

--- app/controllers/storehouses_controller.rb
+++ app/controllers/storehouses_controller.rb
@@ -1,4 +1,18 @@
 class StorehousesController < ApplicationController
+  before_action :set_home_find
+
   def index
   end
+
+
+
+  private
+
+  def set_home_find
+    # @home = Home.find_by(id: params[:home_id])
+    # 調べたコード
+    @home = Home.find(params[:home_id])
+    # Homeテーブルからhome_idカラムを取得 "id"のみだとエラー
+  end
+
 end
