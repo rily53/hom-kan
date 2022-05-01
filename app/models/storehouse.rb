565722e53cd28ec341e9ad9ba1e60fd0c402bc2b
rily53 <98877715+rily53@users.noreply.github.com>
2022-05-02 00:39:17 +0900

    Storehouse-ModelFix

--- app/models/storehouse.rb
+++ app/models/storehouse.rb
@@ -2,10 +2,7 @@ class Storehouse < ApplicationRecord
 
   belongs_to :user
   belongs_to :home
-  has_many_attached :images
 
   validates :folder_name, presence: true
-  validates :file_name, presence: true
-  validate :file_memo
 
 end
