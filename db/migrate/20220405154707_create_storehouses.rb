5be254ce0cea56d2ac409d280508dfee53a1dfd3
rily53 <98877715+rily53@users.noreply.github.com>
2022-05-02 00:38:41 +0900

    Storehouse-DBrollback

--- db/migrate/20220405154707_create_storehouses.rb
+++ db/migrate/20220405154707_create_storehouses.rb
@@ -2,8 +2,6 @@ class CreateStorehouses < ActiveRecord::Migration[6.0]
   def change
     create_table :storehouses do |t|
       t.string :folder_name, null: false
-      t.string :file_name, null: false
-      t.text :file_memo
 
       t.references :user, null: false, foreign_key: true
       t.references :home, null: false, foreign_key: true
